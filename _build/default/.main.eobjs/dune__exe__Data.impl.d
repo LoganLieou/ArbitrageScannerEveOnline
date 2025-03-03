d270c67ed463660d49ae62f7678566f2e4985a4f
loganlieou <logan.lieou@gmail.com>
2025-03-03 00:07:59 -0600

    remove build

--- _build/default/.main.eobjs/dune__exe__Data.impl.d
+++
@@ -1,1 +0,0 @@
-data.ml: Client Cohttp_lwt Cohttp_lwt_unix Lwt Lwt_io Lwt_main Uri
--- _build/default/main.ml
+++
@@ -1,9 +0,0 @@
-open Data
-
-let () =
-  let i = ref 71 in
-  while true do
-    fetch !i;
-    i := !i + 1;
-  done;
-
